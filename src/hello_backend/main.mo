a2bb2a62d1cc4bb598d393956ce507407868a5d9
root <root@DESKTOP-EDTQGNM>
2023-11-17 11:51:16 +0530

    first commit

--- src/hello_backend/main.mo
+++ src/hello_backend/main.mo
@@ -1,5 +1,24 @@
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  type BoxInputData = {
+    box0 : Text;
+    box1 : Text;
+    box2 : Text;
+  };
+
+  public query func changeTurn(turn : Text) : async Text {
+    var result : Text = "";
+    if (turn == "X") {
+      result := "0";
+    } else {
+      result := "X";
+    };
+    return result;
+  };
+
+  public func checkWin(data : BoxInputData) : async Bool {
+    if ((data.box0 == data.box1) and (data.box2 == data.box1) and (data.box0 != "")) {
+      return true;
+    };
+    return false;
   };
 };
--- src/hello_frontend/src/index.js
+++ src/hello_frontend/src/index.js
@@ -1,19 +1,19 @@
-import { hello_backend } from "../../declarations/hello_backend";
+// import { hello_backend } from "../../declarations/hello_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+// document.querySelector("form").addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+//   const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+//   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello_backend.greet(name);
+//   // Interact with foo actor, calling the greet method
+//   const greeting = await hello_backend.greet(name);
 
-  button.removeAttribute("disabled");
+//   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
+//   document.getElementById("greeting").innerText = greeting;
 
-  return false;
-});
+//   return false;
+// });
